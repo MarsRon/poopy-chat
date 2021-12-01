bc364a9862b9e4e5f13e8dbc73b627b498210132
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-01 17:28:01 +0800

    Use envvar port
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
 const path = require('path')
+const dotenv = require('dotenv')
 
-const PORT = 3000
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
